1f3100e540cac0945b873da19054a88567c10b7a
jcmartins81 <jcmartins81@outlook.com>
2021-06-05 15:42:16 -0300

    Configurando mensagens de upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import GlobaStyle from './styles/global'
 import { Container, Content} from "./styles";
 
+import Upload from "./components/Upload";
+
 export default class App extends Component {
   render() {
     return (
     <Container>
       <Content>
-
+        <Upload/>
       </Content>
       <GlobaStyle/>
     </Container>
